7d9add0d2bc591a50dbec275bbf60d61cb9ffedc
HitBlast <67194380+hitblast@users.noreply.github.com>
2023-02-24 13:56:26 +0600

    linted and used match-case conditions instead of if-else to increase performance

--- kaju.py
+++ kaju.py
@@ -1,14 +1,17 @@
-import speech_recognition as sr
-import pyttsx3
-import pywhatkit #to play music or any other media files
+# built-in modules.
 import datetime
-import wikipedia
-import pyjokes
+import os  # open any local app
 import webbrowser
-import os #open any local app
-import requests #request result from online
-from bs4 import BeautifulSoup
+
+# third-party imports
 import fontstyle
+import pyjokes
+import pyttsx3
+import pywhatkit  # to play music or any other media files
+import requests  # request result from online
+import speech_recognition as sr
+import wikipedia
+from bs4 import BeautifulSoup
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
@@ -16,12 +19,11 @@ engine = pyttsx3.init()
 # voices = engine.getProperty('voices')
 # engine.setProperty('voice', voices[1].id)
 
-def talk(text):
-    # engine.say('whats up human, how can i help you')
-    engine.say(text)
+def talk(text: str) -> None:
+    engine.say(text)  # engine.say('whats up human, how can i help you')
     engine.runAndWait()
 
-def take_command():
+def take_command() -> str:
     try:
         with sr.Microphone() as source:
             print('listening...')
@@ -31,103 +33,104 @@ def take_command():
             command = command.lower()
     except:
         pass
+
     return command
 
-def run_assistant():
-    command = take_command()
+def run_assistant() -> None:
+    command: str = take_command()
     print(command)
 
-    if 'play' in command:
-        song = command.replace('play', '')
-        talk('playing ' + song)
-        print(fontstyle.apply('playing',
-                      'bold/Italic/red/UNDERLINE/GREEN_BG'))
-        pywhatkit.playonyt(song)
+    match command:
+        case 'play':
+            song = command.replace('play', '')
+            talk('playing ' + song)
+            print(fontstyle.apply('playing',
+                        'bold/Italic/red/UNDERLINE/GREEN_BG'))
+            pywhatkit.playonyt(song)
 
-    elif 'time' in command:
-        time = datetime.datetime.now().strftime('%I %M %p' )
-        timetext = fontstyle.apply(time, 'bold/Italic/red/GREEN_BG')
-        print(timetext)
-        talk('its' + time)
+        case 'time':
+            time = datetime.datetime.now().strftime('%I %M %p' )
+            timetext = fontstyle.apply(time, 'bold/Italic/red/GREEN_BG')
+            print(timetext)
+            talk('its' + time)
 
-    elif 'who is' in command:
-      person= command.replace('who is' ,'')
-      info= wikipedia.summary(person,1)
-      infotext = fontstyle.apply(info, 'bold/Italic/red/GREEN_BG')
-      print(infotext)
-      talk(info)
+        case 'who is':
+            person= command.replace('who is' ,'')
+            info= wikipedia.summary(person,1)
+            infotext = fontstyle.apply(info, 'bold/Italic/red/GREEN_BG')
+            print(infotext)
+            talk(info)
       
 
-    elif 'what is' in command:
-      person= command.replace('what is' ,'')
-      info= wikipedia.summary(person,1)
-      infotext = fontstyle.apply(info, 'bold/Italic/red/GREEN_BG')
-      print(infotext)
-      talk(info)
+        case 'what is':
+            person= command.replace('what is' ,'')
+            info= wikipedia.summary(person,1)
+            infotext = fontstyle.apply(info, 'bold/Italic/red/GREEN_BG')
+            print(infotext)
+            talk(info)
 
-    elif 'single' in command:
-        
-        printsingle=fontstyle.apply('Sorry I am already in a relationship with wifi', 'bold/Italic/red/GREEN_BG')
-        print(printsingle)
-        talk('Sorry I am already in a relationship with wifi')
+        case 'single':       
+            printsingle=fontstyle.apply('Sorry I am already in a relationship with wifi', 'bold/Italic/red/GREEN_BG')
+            print(printsingle)
+            talk('Sorry I am already in a relationship with wifi')
 
-    elif 'joke' in command:
-        jokes=pyjokes.get_joke()
-        joketext = fontstyle.apply(jokes, 'bold/Italic/red/GREEN_BG')
-        print(joketext)
-        talk(jokes)
+        case 'joke':
+            jokes=pyjokes.get_joke()
+            joketext = fontstyle.apply(jokes, 'bold/Italic/red/GREEN_BG')
+            print(joketext)
+            talk(jokes)
 
-    elif 'open youtube' in command:
-        webbrowser.open("youtube.com")
-        talk('opening')
-        print(fontstyle.apply('Opening',
-                      'bold/Italic/red/UNDERLINE/GREEN_BG'))
+        case 'open youtube':
+            webbrowser.open("youtube.com")
+            talk('opening')
+            print(fontstyle.apply('Opening',
+                        'bold/Italic/red/UNDERLINE/GREEN_BG'))
 
-    elif 'open google' in command:
-        webbrowser.open("google.com")
-        talk('opening')
-        print(fontstyle.apply('Opening',
-                      'bold/Italic/red/UNDERLINE/GREEN_BG'))
+        case 'open google':
+            webbrowser.open("google.com")
+            talk('opening')
+            print(fontstyle.apply('Opening',
+                        'bold/Italic/red/UNDERLINE/GREEN_BG'))
 
-    elif 'open facebook' in command:
-        webbrowser.open("facebook.com")
-        talk('opening')
-        print(fontstyle.apply('Opening',
-                      'bold/Italic/red/UNDERLINE/GREEN_BG'))
+        case 'open facebook':
+            webbrowser.open("facebook.com")
+            talk('opening')
+            print(fontstyle.apply('Opening',
+                        'bold/Italic/red/UNDERLINE/GREEN_BG'))
 
-    elif 'open instagram' in command:
-        webbrowser.open("instagram.com")
-        talk('opening')
-        print(fontstyle.apply('Opening',
-                      'bold/Italic/red/UNDERLINE/GREEN_BG'))
+        case 'open instagram':
+            webbrowser.open("instagram.com")
+            talk('opening')
+            print(fontstyle.apply('Opening',
+                        'bold/Italic/red/UNDERLINE/GREEN_BG'))
 
-    elif 'open browser' in command:
-        CodePath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-        os.startfile(CodePath)
-        print(fontstyle.apply('Opening',
-                      'bold/Italic/red/UNDERLINE/GREEN_BG'))
-        talk('opening')
+        case 'open browser':
+            CodePath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+            os.startfile(CodePath)
+            print(fontstyle.apply('Opening',
+                        'bold/Italic/red/UNDERLINE/GREEN_BG'))
+            talk('opening')
 
-    elif 'weather' in command:
-        search = "temperature"
-        url=f"https://www.google.com/search?q={search}"
-        r= requests.get(url)
-        data= BeautifulSoup(r.text,"html.parser")
-        temp=data.find("div",class_="BNeawe").text
-        print(fontstyle.apply('Current Temperature is',
-                      'bold/Italic/red/UNDERLINE/GREEN_BG'))
-        weathertext=fontstyle.apply(temp, 'bold/Italic/red/GREEN_BG')
-        print(weathertext)
-        talk(f"current temperature is {temp}")
+        case 'weather':
+            search = "temperature"
+            url=f"https://www.google.com/search?q={search}"
+            r= requests.get(url)
+            data= BeautifulSoup(r.text,"html.parser")
+            temp=data.find("div",class_="BNeawe").text
+            print(fontstyle.apply('Current Temperature is',
+                        'bold/Italic/red/UNDERLINE/GREEN_BG'))
+            weathertext=fontstyle.apply(temp, 'bold/Italic/red/GREEN_BG')
+            print(weathertext)
+            talk(f"current temperature is {temp}")
 
-    elif 'thank you' in command:
-        talk('See You Again!')
-        print(fontstyle.apply('See You Again',
-                      'bold/Italic/yellow/UNDERLINE/RED_BG'))
-        exit(run_assistant)
+        case 'thank you':
+            talk('See You Again!')
+            print(fontstyle.apply('See You Again',
+                        'bold/Italic/yellow/UNDERLINE/RED_BG'))
+            exit(run_assistant)
 
-    else:
-        talk('Sorry, Can you repeat?')
+        case _:
+            talk('Sorry, Can you repeat?')
 
 while True:
     run_assistant()
